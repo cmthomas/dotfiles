7d01a1a88853952f4eb7688dde2326b4cffbe72e
Christopher Thomas <cmthomas@cmthomas.us>
2016-03-14 22:18:53 -0700

    Fix xplanet script to work properly on single-display machines.

--- shared/bin/xplanet-cron.sh
+++ shared/bin/xplanet-cron.sh
@@ -7,6 +7,10 @@ rm -f $HOME/Pictures/xplanet-out.png
 
 xplanet -projection rectangular -date_format "%a %e %b %Y %H:%M UT" -geometry 1920x1040 -gmtlabel -labelpos +5+5 -latitude $LATITUDE -longitude $LONGITUDE -num_times 1 -config $HOME/.xplanet/xplanet.conf -output $HOME/Pictures/xplanet.png
 
-montage -tile x1 -geometry x1040 $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet-out.png
+if [ $NUM_DISPLAYS -eq 1 ]; then
+	mv $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet-out.png
+elif [ $NUM_DISPLAYS -eq 2 ]; then
+	montage -tile x1 -geometry x1040 $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet-out.png
+fi
 
 DISPLAY=:0.0 feh --no-xinerama --bg-center $HOME/Pictures/xplanet-out.png
