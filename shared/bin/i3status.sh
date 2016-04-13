877cb04071533719983086ff50ce1ef0dd883615
Christopher Thomas <cmthomas@cmthomas.us>
2016-04-12 20:06:09 -0700

    Factor the display size out into location.sh.

--- shared/bin/i3status.sh
+++ shared/bin/i3status.sh
@@ -2,5 +2,5 @@
 
 i3status -c $HOME/.i3status.conf | while :; do
 	read line
-	echo "$(cat $HOME/.swreport)^fg(#333333)^p(5;-2)^ro(2)^p()^fg()^p(5)$line" || exit 1
+	echo "$(head -n1 $HOME/.swreport)^fg(#333333)^p(5;-2)^ro(2)^p()^fg()^p(5)$line" || exit 1
 done
--- shared/bin/xplanet-cron.sh
+++ shared/bin/xplanet-cron.sh
@@ -5,12 +5,14 @@ rm -f $HOME/Pictures/xplanet-out.png
 
 . $HOME/bin/location.sh
 
-xplanet -projection rectangular -date_format "%a %e %b %Y %H:%M UT" -geometry 1920x1040 -gmtlabel -labelpos +5+5 -latitude $LATITUDE -longitude $LONGITUDE -num_times 1 -config $HOME/.xplanet/xplanet.conf -output $HOME/Pictures/xplanet.png
+IMAGE_Y=`echo "$DISPLAY_Y-40" | bc -l`
+
+xplanet -projection rectangular -date_format "%a %e %b %Y %H:%M UT" -geometry "$DISPLAY_X"x"$IMAGE_Y" -gmtlabel -labelpos +5+5 -latitude $LATITUDE -longitude $LONGITUDE -num_times 1 -config $HOME/.xplanet/xplanet.conf -output $HOME/Pictures/xplanet.png
 
 if [ $NUM_DISPLAYS -eq 1 ]; then
 	mv $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet-out.png
 elif [ $NUM_DISPLAYS -eq 2 ]; then
-	montage -tile x1 -geometry x1040 $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet-out.png
+	montage -tile x1 -geometry x$IMAGE_Y $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet.png $HOME/Pictures/xplanet-out.png
 fi
 
 DISPLAY=:0.0 feh --no-xinerama --bg-center $HOME/Pictures/xplanet-out.png
